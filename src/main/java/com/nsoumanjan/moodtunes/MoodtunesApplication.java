f7861fa9f260343af5e597e536255b82af9ff6c7
Soum8205 <nsoumanjan123@gmail.com>
2026-03-07 00:56:52 +0530

    initial project setup

---
+++ src/main/java/com/nsoumanjan/moodtunes/MoodtunesApplication.java
@@ -0,0 +1,13 @@
+package com.nsoumanjan.moodtunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodtunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodtunesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsoumanjan/moodtunes/MoodtunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsoumanjan.moodtunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodtunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
